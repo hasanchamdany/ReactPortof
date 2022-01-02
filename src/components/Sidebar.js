bcf97b40791dd1bdbbd64f00f35f5902438f3207
hasanchamdany <hasanchamdany@gmail.com>
2022-01-02 15:05:59 +0700

    add sidebar content

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import facebook from "../assets/icons/facebook.svg"
+import instagram from "../assets/icons/instagram.svg"
+import github from "../assets/icons/github.svg"
+import pin from "../assets/icons/pin.svg"
+import tie from "../assets/icons/tie.svg"
+import myavatar from "../assets/myavatar.svg"
 
 const Sidebar = () => {
     return (
-        <h1>Sidebar</h1>
+        <div className="sidebar">
+            <img src={myavatar} alt="avatar" className="sidebar__avatar" />
+            <div className="sidebar__name">Hasan <span>Chamdany</span> </div>
+            <div className="sidebar__item sidebar__title"> Web and Android Developer</div>
+            <a href="!#">
+                <div className="sidebar__item">
+                    <img src={tie} alt="CV" className="sidebar__icon"/> Download CV
+                </div>
+            </a>
+            <figure className="sidebar__social.icons">
+                <a href=""><img src={facebook} alt="Facebook" className="sidebar__icon"/></a>
+                <a href=""><img src={instagram} alt="instagram" className="sidebar__icon"/></a>
+
+            </figure>
+            <div className="contact">
+                <div className="sidebar__item">
+                    <a href=""><img src={github} alt="github" className="sidebar__icon"/>github</a>
+                </div>
+                <div className="sidebar__location">
+                    <img src={pin} alt="location" className="sidebar__icon"/>
+                    Gresik, Indonesia</div>
+                <div className="sidebar__item">hasanchamdany1@gmail.com</div>
+                <div className="sidebar__item">081330666660</div>
+            </div>
+        </div>
     )
 }
 

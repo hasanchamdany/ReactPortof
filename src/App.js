b1526fa228711e5f734f0efacd68aa395cf62625
hasanchamdany <hasanchamdany@gmail.com>
2022-03-04 22:07:40 +0700

    fixing container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
-import React from 'react';
-import { BrowserRouter as Router} from "react-router-dom";
-import {Route} from "react-router-dom";
-import Sidebar from './components/Sidebar';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
-import About from './components/About';
-import TechStack from './components/TechStack';
+import About from "./components/About";
+import TechStack from "./components/TechStack";
 import Main from "./components/Main";
 
 function App() {
@@ -15,15 +15,15 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col-lg-3">
-                <Sidebar />
+              <Sidebar />
             </div>
             <div className="col-lg-9 app__main-content">
-              <Navbar/>
-              <Main/>
-                {/* <Route path="/" components={<About/>}/>
-                  <About />
-                <Route path="/TechStack" components={<TechStack/>}/>
-                  <TechStack/> */}
+              <Navbar />
+              <Main />
+              {/* <Route path="/" components={<About />} /> */}
+              {/* <About /> */}
+              {/* <Route path="/TechStack" components={<TechStack />} /> */}
+              {/* <TechStack /> */}
             </div>
           </div>
         </div>

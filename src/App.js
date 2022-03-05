aea37cdb8c117fbcd07181b5bc96eaf1d732c4cb
hasanchamdany <hasanchamdany@gmail.com>
2022-03-05 15:43:51 +0700

    fixing warning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
-import About from "./components/About";
-import TechStack from "./components/TechStack";
+// import About from "./components/About";
+// import TechStack from "./components/TechStack";
+
+
 import Main from "./components/Main";
 
 function App() {
